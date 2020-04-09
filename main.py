2bcfc804fca839eda1f57c519a2fac6765e64d3b
Moonwalker1996 <54628577+Moonwalker1996@users.noreply.github.com>
2020-04-09 17:40:24 +0400

    Update main.py

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ async def cl(ctx, n = 3):
 @client.command()
 async def rnb(ctx, args, rangei):
 	if (bot_c := random.choice(range(0, (rangeint := int(rangei))))) == (arg := int(args)):
-		await ctx.send('Я знаю!Твое число: ' + args + 'Это было проще простого')
+		await ctx.send('Я знаю!Твое число: ' + args + '\nЭто было проще простого')
 	elif arg > rangeint:
 		await ctx.send('Ты жульничаешь')
 	else:
